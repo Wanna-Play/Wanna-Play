f840133d6a61e4fb94be4080a15698bd16de51e2
ciaorachel <ciaorachel@gmail.com>
2015-10-04 17:27:06 -0500

    updated search results page (game events index). still need some work on this one -- no echo for outputting location address, need dates and times formatted also (they are returned as varchar currently)

--- app/views/game_events/index.blade.php
+++ app/views/game_events/index.blade.php
@@ -6,35 +6,7 @@
 
 @section('content')
 
-<h1>Listing of Game Events</h1>
-{{-- passing in events from GameEventsController Index method; links paginates --}}
-	{{ $events->links() }}
-
-
-	@foreach($events as $event)
-
-	{{-- TO DO:  Add Image Tag 	
-		<h3><strong>Event Image: {{{$gameevent->event_image->event_image}}}</strong></h3> --}}
-
-		<h3><strong>Event Name: {{{$event->event_name}}}</strong></h3>
-		{{-- add sport tags using jQuery plugin tagsinput --}}
-
-
-		<h3><strong>Sport Type: {{{$event->sport->sport}}}</strong></h3>
-		<h3><strong>Gender: {{{$event->gender}}}</strong></h3>
-		<h3><strong>Skill Level: {{{$event->skill_level}}}</strong></h3>
-		<h5><strong>Price: </strong>{{{number_format($event->amount, 2) }}}</h5>
-		<h5><strong>Description: </strong>{{{Str::words($event->description, 20) }}}</h5>
-		<h5><strong>Start Time: </strong>{{$event->start_time}}</h5>
-
-		<h5><strong>End Time: </strong>{{$event->end_time}}</h5>
-
-
-
-<a href="{{{action('GameEventsController@show', $event->id)}}}">View Game Event</a>
-@endforeach
-
-{{-- <div class="container-fluid">
+<div class="container-fluid">
 	<div class="row">
 		<h1 class="page-header noHeaderSpace">Search Results</h1>
 
@@ -85,133 +57,38 @@
 									</div>
 								</div>
 
-								<div class="form-group updateResultsSubmit">
-									<label for="sportResults">End Date</label>
-									<div class="input-group">
-								  		<div class="input-group-addon"><input type="checkbox"></div>
-								  		<input type="text" class="form-control" id="sportResults">
-									</div>
-								</div>
-
-								<div class="form-group updateResultsSubmit">
-									<label for="sportResults">ZIP Code</label>
-									<div class="input-group">
-								  		<div class="input-group-addon"><input type="checkbox"></div>
-								  		<input type="text" class="form-control" id="sportResults">
-									</div>
-								</div>
-
-								<button type="submit" class="btn btn-block updateResultsSubmit">Update results</button>
+								<button type="submit" class="btn btn-block updateResultsSubmit media">Update results</button>
 							</form>
 						</div>
 					</div>
 				</div>
 			</div>
-
-			<div class="nav nav-sidebar">
-				<a href="">View Archived Events</a>
-			</div>
 	    </div>
 	    <div class="col-sm-9 col-md-10 main">
+			
+			{{ $events->links() }}
 
-	    	<h2 class="sub-header noHeaderSpace">Today</h2>
+			@foreach($events as $event)
+	    	<h2 class="sub-header noHeaderSpace">{{$event->start_time}}</h2> {{-- Need this start time formatted like: ->format('l, F jS Y') --}}
 	    	<div class="media">
 				<div class="media-left">
-					<a href="#">
-					  <img class="media-object" src="https://placeholdit.imgix.net/~text?txtsize=23&txt=80%C3%9780&w=80&h=80" alt="...">
+					<a href="{{{action('GameEventsController@show', $event->id)}}}">
+						<img class="media-object" src="{{{$event->event_image}}}" style="height: 80px; width: 80px;">
 					</a>
 				</div>
 				<div class="media-body">
-					<h3 class="noHeaderSpace">Baseball at Brackenridge Park Baseball at Brackenridge Park</h3>
-						<p>123 Broadway St., San Antonio, TX 78209</p>
-						<p>Starts at: 6 p.m. | Ends at 8 p.m.</p>
-						<p>Gender: Mixed | Skill Level: Beginner</p>
-						<p><a href="#" class="btn btn-primary" role="button">See More</a></p>
+					<h3 class="noHeaderSpace">{{{$event->event_name}}}</h3>
+						<p>123 Broadway St., San Antonio, TX 78209</p> {{-- Need to get addresses for events from database --}}
+						<p>Starts at: {{$event->start_time}} | Ends at: {{$event->end_time}}</p> {{-- Need start and end times formatted like: ->format('h:i:s A') --}}
+						<p>Gender: {{{$event->gender}}} | Skill Level: {{{$event->skill_level}}}</p>
+						<p><a href="{{{action('GameEventsController@show', $event->id)}}}" class="btn btn-result" role="button">See More</a></p>
 				</div>
 			</div>
-
-			<div class="media">
-				<div class="media-left">
-					<a href="#">
-					  <img class="media-object" src="https://placeholdit.imgix.net/~text?txtsize=23&txt=80%C3%9780&w=80&h=80" alt="...">
-					</a>
-				</div>
-				<div class="media-body">
-					<h3 class="noHeaderSpace">Baseball at Brackenridge Park Baseball at Brackenridge Park</h3>
-						<p>123 Broadway St., San Antonio, TX 78209</p>
-						<p>Starts at: 6 p.m. | Ends at 8 p.m.</p>
-						<p>Gender: Mixed | Skill Level: Beginner</p>
-						<p><a href="#" class="btn btn-primary" role="button">See More</a></p>
-				</div>
-			</div>
-
-			<h2 class="sub-header noHeaderSpace">Oct. 1st</h2>
-	    	<div class="media">
-				<div class="media-left">
-					<a href="#">
-					  <img class="media-object" src="https://placeholdit.imgix.net/~text?txtsize=23&txt=80%C3%9780&w=80&h=80" alt="...">
-					</a>
-				</div>
-				<div class="media-body">
-					<h3 class="noHeaderSpace">Baseball at Brackenridge Park Baseball at Brackenridge Park</h3>
-						<p>123 Broadway St., San Antonio, TX 78209</p>
-						<p>Starts at: 6 p.m. | Ends at 8 p.m.</p>
-						<p>Gender: Mixed | Skill Level: Beginner</p>
-						<p><a href="#" class="btn btn-primary" role="button">See More</a></p>
-				</div>
-			</div>
-
-			<div class="media">
-				<div class="media-left">
-					<a href="#">
-					  <img class="media-object" src="https://placeholdit.imgix.net/~text?txtsize=23&txt=80%C3%9780&w=80&h=80" alt="...">
-					</a>
-				</div>
-				<div class="media-body">
-					<h3 class="noHeaderSpace">Baseball at Brackenridge Park Baseball at Brackenridge Park</h3>
-						<p>123 Broadway St., San Antonio, TX 78209</p>
-						<p>Starts at: 6 p.m. | Ends at 8 p.m.</p>
-						<p>Gender: Mixed | Skill Level: Beginner</p>
-						<p><a href="#" class="btn btn-primary" role="button">See More</a></p>
-				</div>
-			</div>
-
-
-			<h2 class="sub-header noHeaderSpace">Oct. 15th</h2>
-	    	<div class="media">
-				<div class="media-left">
-					<a href="#">
-					  <img class="media-object" src="https://placeholdit.imgix.net/~text?txtsize=23&txt=80%C3%9780&w=80&h=80" alt="...">
-					</a>
-				</div>
-				<div class="media-body">
-					<h3 class="noHeaderSpace">Baseball at Brackenridge Park Baseball at Brackenridge Park</h3>
-						<p>123 Broadway St., San Antonio, TX 78209</p>
-						<p>Starts at: 6 p.m. | Ends at 8 p.m.</p>
-						<p>Gender: Mixed | Skill Level: Beginner</p>
-						<p><a href="#" class="btn btn-primary" role="button">See More</a></p>
-				</div>
-			</div>
-
-			<div class="media">
-				<div class="media-left">
-					<a href="#">
-					  <img class="media-object" src="https://placeholdit.imgix.net/~text?txtsize=23&txt=80%C3%9780&w=80&h=80" alt="...">
-					</a>
-				</div>
-				<div class="media-body">
-					<h3 class="noHeaderSpace">Baseball at Brackenridge Park Baseball at Brackenridge Park</h3>
-						<p>123 Broadway St., San Antonio, TX 78209</p>
-						<p>Starts at: 6 p.m. | Ends at 8 p.m.</p>
-						<p>Gender: Mixed | Skill Level: Beginner</p>
-						<p><a href="#" class="btn btn-primary" role="button">See More</a></p>
-				</div>
-			</div>
-
+			@endforeach
 
 	    </div>
 	</div>
-</div> --}}
+</div>
 
 
 
